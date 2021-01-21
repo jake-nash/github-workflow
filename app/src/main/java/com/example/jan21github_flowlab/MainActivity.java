948e10405eb6a007a2bebb49ea8e1895b1c085df
Jacob Nash <nashjr@g.cofc.edu>
2021-01-21 18:30:23 -0500

    github flow lab

---
+++ app/src/main/java/com/example/jan21github_flowlab/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.jan21github_flowlab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
